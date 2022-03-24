d107c23217de0314ede833a6a27147560a23f899
Max Henneberg <henneberg.maximilian@googlemail.com>
2022-03-24 11:02:38 +0100

    [MaHe] Added Cors

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const cors = require('cors')
 const userStudyRouter = require('./routes/userstudy');
 const app = express();
 app.use(cors())
+app.use(express.json())
 
-app.get('/', (req, res) => res.send('Home Page Route 1'));
+app.get('/', (req, res) => res.send('Home Page Route 2'));
 
 app.get('/about', (req, res) => res.send('About Page Route'));
 
