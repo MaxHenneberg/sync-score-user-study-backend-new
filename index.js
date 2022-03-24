ede7d4620b6206d974b56bda535d895b6a41b04f
Max Henneberg <henneberg.maximilian@googlemail.com>
2022-03-24 09:57:31 +0100

    [MaHe] Removed some unused routes

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ app.get('/', (req, res) => res.send('Home Page Route'));
 
 app.get('/about', (req, res) => res.send('About Page Route'));
 
-app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
-
-app.get('/contact', (req, res) => res.send('Contact Page Route'));
 app.use('/userstudy', userStudyRouter);
 const port = process.env.PORT || 3000;
 
