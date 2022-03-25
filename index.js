2198411b1948e4ee888124112ecc4bce36558c32
Max Henneberg <henneberg.maximilian@googlemail.com>
2022-03-25 11:41:19 +0100

    [MaHe] First final release

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const express = require('express');
 const cors = require('cors')
 
 const userStudyRouter = require('./routes/userstudy');
+const userDataRouter = require('./routes/userdata')
 const app = express();
 app.use(cors({origin:"*"}))
 app.use(express.json())
 
-app.get('/', (req, res) => res.send('Home Page Route 2'));
+app.get('/', (req, res) => res.send('Home Page Route 5'));
 
 app.get('/about', (req, res) => res.send('About Page Route'));
 
 app.use('/userstudy', userStudyRouter);
+app.use('/userdata', userDataRouter);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`))
--- routes/userdata.js
+++ routes/userdata.js
@@ -8,28 +8,31 @@ const sns = new AWS.SNS();
 const ddb = new AWS.DynamoDB();
 
 const snsTopic =  process.env.TOPIC_USER_STUDY;
-const TABLE_SYNC_SCORE = process.env.TABLE_SYNC_SCORE;
-console.log(`SyncScoreTable: ${TABLE_SYNC_SCORE}`)
+const TABLE_USER_DATA = process.env.TABLE_USER_DATA;
+console.log(`UserDataTable: ${TABLE_USER_DATA}`)
 
 /* GET users listing. */
 router.post('/store', function (req, res, next) {
-    console.log(`Received Post request for ${req.body.study}`);
+    console.log(`Received UserData for ${req.body.runId}`);
     storeInDb(req.body);
     res.send('respond with a resource');
 });
 router.get('/test', (req, res) => {
-    res.send('UserData worked')
+    res.send('UserStudy worked')
 });
 
 function storeInDb(objectToBeStored) {
     const item = {
         'runId': {'S': `${objectToBeStored.runId}`},
-        'study': {'S': `${objectToBeStored.study}`},
-        'syncScore': {'S': objectToBeStored.syncScore}
+        'age': {'N': `${objectToBeStored.age}`},
+        'gender': {'S': objectToBeStored.gender},
+        'countryLiving': {'S': objectToBeStored.countryLiving},
+        'countryGrownUp': {'S': objectToBeStored.countryGrownUp},
+        'education': {'S': objectToBeStored.education}
     };
     console.log(`Item: ${JSON.stringify(item)}`)
     ddb.putItem({
-        'TableName': TABLE_SYNC_SCORE,
+        'TableName': TABLE_USER_DATA,
         'Item': item
     }, (err, data) => {
         const message = formatMessage(objectToBeStored);
@@ -37,7 +40,7 @@ function storeInDb(objectToBeStored) {
             console.log(err);
             sns.publish({
                 'Message': JSON.stringify(err),
-                'Subject': 'ERROR: Storing new UserStudy',
+                'Subject': 'ERROR: Storing new UserData',
                 'TopicArn': snsTopic
             }, function (err, data) {
                 if (err) {
@@ -63,8 +66,8 @@ function storeInDb(objectToBeStored) {
     })
 }
 
-function formatMessage(body) {
-    return `Received new UserStudy :${JSON.stringify(body)}`;
+function formatMessage(body){
+    return `Received UserData:\n ${JSON.stringify(body)}`
 }
 
 module.exports = router;
